2ea567a4e07b7f093173a95d213ba5f7ce68ef32
engusmaster <dtrofimenko.523@gmail.com>
2022-05-19 22:33:04 +0300

    Добавьте файлы проекта.

---
+++ TrofimenkoPr15/TrofimenkoPr15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using TrofimenkoPr15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TrofimenkoPr15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
